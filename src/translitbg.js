2802e2dd6206a0c11b01694e000656e538bab416
Tsvetomir Stoev <tsvetomir@boleron.com>
2023-06-26 12:42:40 +0300

    TRBG-1 Fix for all caps SH letter

--- src/translitbg.js
+++ src/translitbg.js
@@ -1,99 +1,100 @@
-(function() {
+(function () {
   "use strict";
   var root = this;
   var previous = root.translitbg;
 
   var translitbg = {
-    go: function(text) {
+    go: function (text) {
       return transliterate(text, TranslitBGModes.STREAMLINED);
     },
-    noConflict: function() {
+    noConflict: function () {
       root.translitbg = previous;
       return this;
-    }
+    },
   };
 
   var TranslitBGModes = {
-    STREAMLINED : { // Възстановяването на оригиналната дума не е водещ принцип!
-      tokens : {
+    STREAMLINED: {
+      // Възстановяването на оригиналната дума не е водещ принцип!
+      tokens: {
         // 'дж' : 'dzh',
         // 'дз' : 'dz',
         // 'ьо' : 'yo',
         // 'йо' : 'yo',
         // Буквеното съчетание „ия“, когато е в края на думата, се изписва и предава чрез „ia“.
-        ia : {
-          'ия' : 'ia',
-          'Ия' : 'Ia',
-          'иЯ' : 'iA',
-          'ИЯ' : 'IA',
-        }
+        ia: {
+          ия: "ia",
+          Ия: "Ia",
+          иЯ: "iA",
+          ИЯ: "IA",
+        },
       },
       cyr2lat: {
         // lower case
-        'а' : 'a',
-        'б' : 'b',
-        'в' : 'v',
-        'г' : 'g',
-        'д' : 'd',
-        'е' : 'e',
-        'ж' : 'zh',
-        'з' : 'z',
-        'и' : 'i',
-        'ѝ' : 'i',
-        'й' : 'y',
-        'к' : 'k',
-        'л' : 'l',
-        'м' : 'm',
-        'н' : 'n',
-        'о' : 'o',
-        'п' : 'p',
-        'р' : 'r',
-        'с' : 's',
-        'т' : 't',
-        'у' : 'u',
-        'ф' : 'f',
-        'х' : 'h',
-        'ц' : 'ts',
-        'ч' : 'ch',
-        'ш' : 'sh',
-        'щ' : 'sht',
-        'ъ' : 'a',
-        'ь' : 'y',
-        'ю' : 'yu',
-        'я' : 'ya',
+        а: "a",
+        б: "b",
+        в: "v",
+        г: "g",
+        д: "d",
+        е: "e",
+        ж: "zh",
+        з: "z",
+        и: "i",
+        ѝ: "i",
+        й: "y",
+        к: "k",
+        л: "l",
+        м: "m",
+        н: "n",
+        о: "o",
+        п: "p",
+        р: "r",
+        с: "s",
+        т: "t",
+        у: "u",
+        ф: "f",
+        х: "h",
+        ц: "ts",
+        ч: "ch",
+        ш: "sh",
+        щ: "sht",
+        ъ: "a",
+        ь: "y",
+        ю: "yu",
+        я: "ya",
         // upper case
-        'А' : 'A',
-        'Б' : 'B',
-        'В' : 'V',
-        'Г' : 'G',
-        'Д' : 'D',
-        'Е' : 'E',
-        'Ж' : 'Zh',
-        'З' : 'Z',
-        'И' : 'I',
-        'Ѝ' : 'I',
-        'Й' : 'Y',
-        'К' : 'K',
-        'Л' : 'L',
-        'М' : 'M',
-        'Н' : 'N',
-        'О' : 'O',
-        'П' : 'P',
-        'Р' : 'R',
-        'С' : 'S',
-        'Т' : 'T',
-        'У' : 'U',
-        'Ф' : 'F',
-        'Х' : 'H',
-        'Ц' : 'Ts',
-        'Ч' : 'Ch',
-        'Ш' : 'SH',
-        'Щ' : 'Sht',
-        'Ъ' : 'A',
-        'Ь' : 'Y',
-        'Ю' : 'Yu',
-        'Я' : 'Ya'
-      }
+        А: "A",
+        Б: "B",
+        В: "V",
+        Г: "G",
+        Д: "D",
+        Е: "E",
+        Ж: "Zh",
+        З: "Z",
+        И: "I",
+        Ѝ: "I",
+        Й: "Y",
+        К: "K",
+        Л: "L",
+        М: "M",
+        Н: "N",
+        О: "O",
+        П: "P",
+        Р: "R",
+        С: "S",
+        Т: "T",
+        У: "U",
+        Ф: "F",
+        Х: "H",
+        Ц: "Ts",
+        Ч: "Ch",
+        Ш: "Sh",
+        Щ: "Sht",
+        Ъ: "A",
+        Ь: "Y",
+        Ю: "Yu",
+        Я: "Ya",
+      },
     },
     // // TODO: БДС ISO 9:2001
     // BDS_ISO9_2001 : {},
@@ -103,7 +104,7 @@
 
   function transliterate(text, mode) {
     var result = [];
-    var chars = text.split('');
+    var chars = text.split("");
 
     for (var i = 0; i < chars.length; i++) {
       var cur = chars[i];
@@ -129,19 +130,18 @@
       }
     }
 
-    return result.join('');
+    return result.join("");
   }
 
   /**
    * Exports
    */
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
+  if (typeof exports !== "undefined") {
+    if (typeof module !== "undefined" && module.exports) {
       exports = module.exports = translitbg;
     }
     exports.translitbg = translitbg;
   } else {
     root.translitbg = translitbg;
   }
-
 }).call(this);
